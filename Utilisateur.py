9595fb95b032a2cd75925180b54c3e9bb68325ef
kylianmv <he202475@students.ephec.be>
2025-11-26 14:06:10 +0100

    Update Utilisateur.py
    Ajout du mdp crypté

--- Utilisateur.py
+++ Utilisateur.py
@@ -1,11 +1,21 @@
+import hashlib #pour hasher le mdp
 class Utilisateur:
     def __init__(self, nomUtilisateur, prenomUtilisateur, mdpUtilisateur, idUtilisateur):
         self.__nom = nomUtilisateur
         self.__prenom = prenomUtilisateur
         self.__mdp = mdpUtilisateur
+        self.__mdp_hash = self._hasher_mdp(mdpUtilisateur)
         self.__id = idUtilisateur
         self.__permission = "user"
+        
+    def _hasher_mdp(self, mdp):
+        # Hashage simple (SHA-256)
+        return hashlib.sha256(mdp.encode()).hexdigest()
 
+    def verifier_mdp(self, mdp_a_verifier):
+        # On hash l'entrée et on compare avec le hash stocké
+        return self._hasher_mdp(mdp_a_verifier) == self.__mdp_hash
+        
     @property
     def permission(self):
         return self.__permission
