8ddbcd7de868a0c3b708eb44a51e0900ac57a034
lucas-Dsk <HE202497@students.ephec.be>
2025-12-03 09:52:27 +0000

    ajout de la fonction deconnecterUtilisateur et reflexion sur les dernières fonctions

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from Reservation import Reservation
+from Reservations import Reservation
+from main import Main
 
 class Application : #construction de base de la classe application
+
+    listeReservation=[]
+
     def __init__(self,utilisateurConnecte):
         self.__utilisateur = utilisateurConnecte 
 
@@ -8,6 +12,19 @@ class Application : #construction de base de la classe application
     def utilisateur(self):
         return self.__utilisateur
 
-    
+    @staticmethod
+    def deconnecterUtilisateur():
+        Main.utilisateurConnecte=None
+        print("vous êtes maintenant déconnecter")
+        Main.application()
+
+    @staticmethod
+    def ajouterReservation():
+        pass
+
+    @staticmethod
+    def voirReservation():
+        pass
+
 
     
